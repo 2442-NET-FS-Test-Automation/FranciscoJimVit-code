a154c8f5f60f0ef2bc7f088dc6f9fb7c4fe4ae23
FraJimVit <francisco.jmz.vital@gmail.com>
2026-07-08 11:43:07 -0600

    DTOs (Data Transfer Objects), controllers

---
+++ 00 - Demos/EF-REST-SOAP-ASP/library-api-demo/Library.ControllerApi/Services/IInventoryService.cs
@@ -0,0 +1,12 @@
+using Library.ControllerApi.DTOs;
+using Library.Data.Entities;
+
+namespace Library.ControllerApi.Services;
+
+public interface IInventoryService
+{
+    Task<IReadOnlyList<InventoryItem>> AllAsync();
+    Task<InventoryItem?> BySkuAsync(string sku);
+    public Task<InventoryItem> AddAsync(InventoryCreateDto newInv);
+    public Task<bool> RemoveAsync(string sku);
+}
